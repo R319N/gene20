6639ad0fb72e13fa866d5bd8e4f1c6d7037bb9f6
Wilfred Reign <wilfredr319n@gmail.com>
2026-08-28 23:18:22 +0200

    new2

--- components/layouts/ClientLayout.tsx
+++ components/layouts/ClientLayout.tsx
@@ -8,7 +8,7 @@ import Typography from "@mui/material/Typography";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
-import SiteTransition from "./SiteTransition";
+// import SiteTransition from "./SiteTransition";
 import uLwandleTheme from "@/assets/theme/uLwandleTheme";
 import NavigationBar from "../navigation/NavigationBar";
 import FooterSection from "@/sections/FooterSection";
